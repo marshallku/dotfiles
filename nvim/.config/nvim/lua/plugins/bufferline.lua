143c51d9ca8f372001d54623bc77cce53c9c8f09
Marshall Ku <marshall@kakao.com>
2026-02-20 09:56:36 +0900

    Remove buffer line

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,23 +1,4 @@
 return {
     "akinsho/bufferline.nvim",
-    dependencies = {"nvim-tree/nvim-web-devicons"},
-    version = "*",
-    config = function()
-        require("bufferline").setup({
-            options = {
-                mode = "buffers",
-                separator_style = "slant",
-                always_show_bufferline = false,
-                diagnostics = "nvim_lsp",
-                offsets = {
-                    {
-                        filetype = "neo-tree",
-                        text = "File Explorer",
-                        highlight = "Directory",
-                        text_align = "left"
-                    }
-                }
-            }
-        })
-    end
+    enabled = false,
 }
