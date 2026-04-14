8cec0cc43e8485ab92042b88128c932b0a9412ed
Marshall Ku <marshall@kakao.com>
2026-04-14 12:13:14 +0900

    Add auto session

---
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -0,0 +1,10 @@
+return {
+	"rmagatti/auto-session",
+	lazy = false,
+	opts = {
+		suppressed_dirs = { "~/", "~/Downloads", "/tmp" },
+		auto_restore = true,
+		auto_save = true,
+		use_git_branch = true,
+	},
+}
