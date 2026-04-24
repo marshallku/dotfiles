acfbc1cbeb8a7ad7a45781e1a50899c016f77728
Marshall Ku <marshall@kakao.com>
2026-04-25 07:55:56 +0900

    Fix tree sitter config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,14 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
     lazy = false,
-    dependencies = {"nvim-treesitter/nvim-treesitter-textobjects"},
+    dependencies = {
+        {"nvim-treesitter/nvim-treesitter-textobjects", branch = "main"}
+    },
     config = function()
-        -- nvim-treesitter v2.x: setup() is optional (only needed for custom install_dir)
         require("nvim-treesitter").setup()
 
-        -- Ensure parsers are installed (async, no-op if already installed)
         require("nvim-treesitter").install({
             "typescript", "tsx", "javascript", "rust", "go", "python",
             "php", "lua", "vim", "vimdoc", "html", "css", "scss", "json",
@@ -15,7 +16,6 @@ return {
             "dockerfile", "gitignore", "graphql"
         })
 
-        -- Text objects (using nvim-treesitter-textobjects v2 API)
         require("nvim-treesitter-textobjects").setup({
             select = {
                 lookahead = true,
@@ -26,6 +26,14 @@ return {
                     ["ic"] = "@class.inner",
                 },
             },
+        })
+
+        -- v2 (main) drops the built-in highlight/indent modules: enable per buffer
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function(args)
+                if not pcall(vim.treesitter.start, args.buf) then return end
+                vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
         })
     end
 }
