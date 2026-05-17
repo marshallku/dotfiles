4ae4b1c59bcbb3d708fee236a66b2186d181261c
Marshall Ku <marshall@kakao.com>
2026-05-17 13:07:03 +0900

    Fix auto session

--- nvim/.config/nvim/lua/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -7,5 +7,15 @@ return {
 		auto_restore = true,
 		auto_save = true,
 		use_git_branch = true,
+		bypass_save_filetypes = { "neo-tree" },
+		pre_save_cmds = {
+			function()
+				for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+					if vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].filetype == "neo-tree" then
+						pcall(vim.api.nvim_buf_delete, buf, { force = true })
+					end
+				end
+			end,
+		},
 	},
 }
